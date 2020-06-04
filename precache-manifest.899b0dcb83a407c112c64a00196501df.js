65507854b0e87e19e9a56e94732968dd357eea0f
Manimaran N <manimaran@dexlock.com>
2020-06-05 01:16:27 +0530

    Updates

---
+++ precache-manifest.899b0dcb83a407c112c64a00196501df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "243d5388ee331affa0972dc7bc8fea87",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "56acd69c0ca2e9c1d1cc",
+    "url": "/portfolio/static/css/main.37ec4897.chunk.css"
+  },
+  {
+    "revision": "38ac70a9e0b998e32223",
+    "url": "/portfolio/static/js/2.2bf516c8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.2bf516c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56acd69c0ca2e9c1d1cc",
+    "url": "/portfolio/static/js/main.212fcfaf.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "0040798942c92fc885a3c9437204871f",
+    "url": "/portfolio/static/media/header-image.00407989.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
